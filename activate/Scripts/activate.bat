4df8d7e2e9962c24b65c632cb286e7b6952b01e9
sepdiba <sepdiba@gmail.com>
2018-12-08 03:55:26 -0500

    Added files and changes required for deployment to heroku

--- activate/Scripts/activate.bat
+++ activate/Scripts/activate.bat
@@ -1,5 +1,5 @@
 @echo off
-set "VIRTUAL_ENV=C:\Users\sepid\django_test\mysite\activate"
+set "VIRTUAL_ENV=C:\Users\sepid\mysite\activate"
 
 if defined _OLD_VIRTUAL_PROMPT (
     set "PROMPT=%_OLD_VIRTUAL_PROMPT%"
